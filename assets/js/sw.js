7d1e0ab51203e2889ca3555ae7b73e4ee181c20d
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-03-23 01:58:26 +0100

    formsubmit.co technology has been implemented to contact.html. Submited text has been sent successfully on my gmail account

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,13 +1,13 @@
 const staticCacheName = 'site-static-v1';
 const assets = [
-    '/Portfolio/',
-    '/Portfolio/index.html',
-    '/Portfolio/contact.html',
-    '/Portfolio/work.html',
-    '/Portfolio/404.html',
+    '/Resume/',
+    '/Resume/index.html',
+    '/Resume/contact.html',
+    '/Resume/work.html',
+    '/Resume/404.html',
     '/style.css',
-    '/Portfolio/assets/img/mobile.png',
-    '/Portfolio/assets/img/favicon.png',
+    '/Resume/assets/img/mobile.png',
+    '/Resume/assets/img/favicon.png',
     'https://fonts.googleapis.com/css?family=Lato:300,400,700'
 ];
 // install event
